9eb5ca646369517bc47314f83314f7d3fb16c926
빵쟁이 <35024143+hansae167@users.noreply.github.com>
2022-09-29 17:24:47 +0900

    loginPageTest

---
+++ UiTest/UiTest.java
@@ -0,0 +1,12 @@
+package UiTest;
+
+public class UiTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        myForm test = new myForm();
+        test.setVisible(true);
+
+    }
+}
+
